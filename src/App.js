53cfe5666565e1364a92f1b97a25161c25b356e0
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-12-06 11:59:41 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/iframe-has-title */
+/* eslint-disable no-unused-vars */
+/* eslint-disable default-case */
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+function New(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap-item wrap-item-new">
+      <span className="label">New!</span>
+      {props.children}
     </div>
   );
 }
 
-export default App;
+function Popular(props) {
+  return (
+    <div className="wrap-item wrap-item-popular">
+      <span className="label">Popular!</span>
+      {props.children}
+    </div>
+  );
+}
+
+function Article(props) {
+  return (
+    <div className="item item-article">
+      <h3>
+        <a href="#">{props.title}</a>
+      </h3>
+      <p className="views">Прочтений: {props.views}</p>
+    </div>
+  );
+}
+
+function Video(props) {
+  return (
+    <div className="item item-video">
+      <iframe
+        src={props.url}
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+      ></iframe>
+      <p className="views">Просмотров: {props.views}</p>
+    </div>
+  );
+}
+
+function DisplayBlock(Component) {
+  return class extends React.Component {
+    render() {
+      console.log(this.props.views);
+      if (this.props.views > 1000) {
+        return (
+          <Popular>
+            <Component {...this.props} />
+          </Popular>
+        );
+      } else if (this.props.views < 100) {
+        return (
+          <New>
+            <Component {...this.props} />
+          </New>
+        );
+      }
+      return <Component {...this.props} />;
+    }
+  };
+}
+
+function List(props) {
+  // eslint-disable-next-line array-callback-return
+  return props.list.map((item) => {
+    switch (item.type) {
+      case "video":
+        const BlockVideo = DisplayBlock(Video);
+        return (
+          <BlockVideo {...item} />
+        );
+
+      case "article":
+        const BlockArticle = DisplayBlock(Article);
+        return (
+          <BlockArticle {...item} />
+        );
+    }
+  });
+}
+
+export default function App() {
+  const [list, setList] = useState([
+    {
+      type: "video",
+      url:
+        "https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0",
+      views: 50,
+    },
+    {
+      type: "video",
+      url:
+        "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
+      views: 12,
+    },
+    {
+      type: "article",
+      title: "Невероятные события в неизвестном поселке...",
+      views: 175,
+    },
+    {
+      type: "article",
+      title: "Секретные данные были раскрыты!",
+      views: 1532,
+    },
+    {
+      type: "video",
+      url:
+        "https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0",
+      views: 4253,
+    },
+    {
+      type: "article",
+      title: "Кот Бегемот обладает невероятной...",
+      views: 12,
+    },
+  ]);
+
+  return <List list={list} />;
+}
